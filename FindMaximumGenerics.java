d8e4ef4306b1dfda7354fe8464a6387fbad2cb2e
PayaLsRaut <payalraut107@gmail.com>
2022-06-18 21:00:14 +0530

    Extend the max method using generics Method

--- FindMaximumGenerics.java
+++ FindMaximumGenerics.java
@@ -1,20 +1,36 @@
 package com.bridgelabz;
 
-public class FindMaximumGenerics {
-    public static <T extends Comparable<T>> T maximum (T x, T y, T z ){
-        T max =x;      //assume x is max
-        if (y.compareTo(max) > 0)
-            max = y;
-        if (z.compareTo(max) > 0)
-            max = z;
-        return max;
+public class FindMaximumGenerics<T extends Comparable<T>>
+{
+    T x;
+    T y;
+    T z;
+
+    FindMaximumGenerics(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public T maximum (){
+
+        if (x.compareTo(y) > 0) {
+            return x;
+        } else if (y.compareTo(z) > 0) {
+            return y;
+        }else{
+            return z;
+        }
     }
 
     public static void main(String[] args) {
+        FindMaximumGenerics Integer = new FindMaximumGenerics(10, 20, 30);
+        System.out.println("Maximum number out of three integers is: " + Integer.maximum());
 
-        System.out.println(maximum(10,20,30));
-        System.out.println(maximum(3.5f, 6.5f, 9.5f));
-        System.out.println(maximum("Apple", "Peach", "Banana"));
+        FindMaximumGenerics Float = new FindMaximumGenerics(3.5f, 6.5f, 9.5f);
+        System.out.println("Maximum number out of three float is: "    +  Float.maximum());
+
+        FindMaximumGenerics String = new FindMaximumGenerics("Apple", "Peach", "Banana");
+        System.out.println("Maximum number out of three string is: "   +  String.maximum());
     }
 }
-
