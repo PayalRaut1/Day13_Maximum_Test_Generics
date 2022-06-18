69634bed5a8b30f26461d8c47e3cafd0be9913ed
PayaLsRaut <payalraut107@gmail.com>
2022-06-18 20:18:45 +0530

    Given 3 strings and find the maximum

--- Generic.java
+++ Generic.java
@@ -17,9 +17,18 @@ public class Generic {
             max = z;
         return max;
     }
+    public static String stringMaximum(String x, String y, String z) {
+        String max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+        if (z.compareTo(max) > 0)
+            max = z;
+        return max;
+    }
     public static void main(String[] args) {
 
         System.out.println(maximum(10,20,30));
         System.out.println(floatMaximum(3.5f, 6.5f, 9.5f));
+        System.out.println(stringMaximum("Apple", "Peach", "Banana"));
     }
 }
