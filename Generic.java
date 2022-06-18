f86e36744d8fa89c6a8201cb171da05879437510
PayaLsRaut <payalraut107@gmail.com>
2022-06-18 19:59:22 +0530

    Added: Given 3 floats and find the maximum

--- Generic.java
+++ Generic.java
@@ -1,8 +1,7 @@
 package com.bridgelabz;
 
 public class Generic {
-    public static int maximum(Integer x, Integer y, Integer z)
-    {
+    public static int maximum(Integer x, Integer y, Integer z) {
         Integer max = x;
         if (y.compareTo(max) > 0)
             max =y;
@@ -10,10 +9,17 @@ public class Generic {
             max = z;
         return max;
     }
-    public static void main(String[] args)
-    {
-        // Integer[] intArray = {3,5,6};
+    public static float floatMaximum(Float x, Float y, Float z) {
+        Float max = x;
+        if (y.compareTo(max) > 0 )
+            max = y;
+        if (z.compareTo(max) > 0)
+            max = z;
+        return max;
+    }
+    public static void main(String[] args) {
+
         System.out.println(maximum(10,20,30));
+        System.out.println(floatMaximum(3.5f, 6.5f, 9.5f));
     }
 }
-
